19da267a5e4a518c4bb75749ea84d5d5df38cb6b
Pedro Mendoza <pedro.mendoza2482@gmail.com>
2020-10-08 08:58:24 -0600

    release1 angular2-signature

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild('signatureCanvas', {static: true}) signaturePad: SignaturePad;
+
+  private signaturePadOptions: Object = { // passed through to szimek/signature_pad constructor
+    'minWidth': 1,
+    'backgroundColor': '#ffffff',
+    'canvasWidth': 500,
+    'canvasHeight': 300
+  };
+
   constructor() {}
+
+  drawStart() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('begin drawing');
+  }
+
+  drawComplete() {
+    // will be notified of szimek/signature_pad's onEnd event
+   // console.log(this.signaturePad.toDataURL());
+
+  }
+
+  clear(){
+
+    this.signaturePad.clear();
+  }
 
 }
